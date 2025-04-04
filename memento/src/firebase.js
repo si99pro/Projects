d265a2298bc83829202ba34585d8a91572bda636
Shahidul Islam <170623757+si99pro@users.noreply.github.com>
2025-04-04 22:38:48 +0600

    db update

--- memento/src/firebase.js
+++ memento/src/firebase.js
@@ -5,15 +5,15 @@ import { getFirestore, doc, updateDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC1i7yW6N2gGNXhlD2HAO-2PeQyfVU37_g", // RESTRICT THIS API KEY!
-    authDomain: "si99-access.firebaseapp.com",
-    databaseURL: "https://si99-access-default-rtdb.firebaseio.com",
-    projectId: "si99-access",
-    storageBucket: "si99-access.firebasestorage.app",
-    messagingSenderId: "633275206043",
-    appId: "1:633275206043:web:595c284e6e0732a8bcb93e",
-    measurementId: "G-FXRN61FTYY"
-  };
+  apiKey: "AIzaSyC_8DotsbDkaJyJPrcs5juFae8saZ61M3Y",
+  authDomain: "univerbase.firebaseapp.com",
+  databaseURL: "https://univerbase-default-rtdb.firebaseio.com/",
+  projectId: "univerbase",
+  storageBucket: "univerbase.firebasestorage.app",
+  messagingSenderId: "357081716576",
+  appId: "1:357081716576:web:ec3758fd748c14cf10f005",
+  measurementId: "G-SEVR8H54QJ"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
